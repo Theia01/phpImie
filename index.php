baf450133af1a454895c239370476e263ad1705e
Theia01 <45165141+Theia01@users.noreply.github.com>
2018-12-04 12:18:03 +0100

    John Snow peut se présenter en anglais ~ woaaaah

--- index.php
+++ index.php
@@ -4,16 +4,30 @@ class Person {
 	private $firstname;
 	private $lastname;
 
+	/*Creation d'un mutateur*/
 	public function setName($pFirstName, $pLastname){
 		$this->firstname = $pFirstName;
 		$this->lastname = $pLastname;
+	}
+
+	/*Creation d'un getteur (accesseur)*/
+	public function getName(){
+		return $this->firstname;
+	}
+
+	/*John se presente*/
+	public function speak(){
+		return "Hello, my name is ".$this->firstname." ".$this->lastname;
 	}
 }
 $person1 = new Person;
 //$person1->firstname = "John";
 //$person1->lastname = "Snow";
 $person1->setName("John", "Snow");
-var_dump($person1);
+//var_dump($person1->getName());
+var_dump($person1->speak());
 
-$person2 = new Person;
-var_dump($person2);
+
+//$person2 = new Person;
+//$person2->setName("Aria", "Stark");
+//var_dump($person2);
