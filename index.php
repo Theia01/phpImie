ae4c809bd1cf08665c80e851b7c750ecd0ccfeee
Theia01 <45165141+Theia01@users.noreply.github.com>
2018-12-04 11:44:48 +0100

    Creation d'une class personne avec ses instances

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php 
 
-echo "coucou";
+class Person {
+	private $firstname;
+	private $lastname;
+
+	public function setName($pFirstName, $pLastname){
+		$this->firstname = $pFirstName;
+		$this->lastname = $pLastname;
+	}
+}
+$person1 = new Person;
+//$person1->firstname = "John";
+//$person1->lastname = "Snow";
+$person1->setName("John", "Snow");
+var_dump($person1);
+
+$person2 = new Person;
+var_dump($person2);
