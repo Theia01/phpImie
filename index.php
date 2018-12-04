8b22a1a5fcbaf4ab11819eab53db38fc0b5ce887
Theia01 <45165141+Theia01@users.noreply.github.com>
2018-12-04 14:41:51 +0100

    La personne a un chien

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 class Person {
 	private $firstname;
 	private $lastname;
+	private $wolf;
 
 	public function __construct($pFirstName, $pLastname){
 		$this->firstname = $pFirstName;
@@ -10,7 +11,7 @@ class Person {
 	}
 
 	/*Creation d'un mutateur*/
-	public function setName($pFirstName, $pLastname){
+	public function setName(string $pFirstName, string $pLastname){
 		$this->firstname = $pFirstName;
 		$this->lastname = $pLastname;
 	}
@@ -21,39 +22,53 @@ class Person {
 	}
 
 	/*John se presente*/
-	public function speak(){
-		return "Hello, my name is ".$this->firstname." ".$this->lastname;
+	public function speak(): string{
+		return "Hello, my name is ".$this->firstname." ".$this->lastname." et mon loup se nomme ".$this->wolf->getNameWolf();
+	}
+
+	public function meetWolf($pWolf){
+		$this->wolf = $pWolf;
 	}
 }
 
 class Wolf{
 	private $nameWolf;
 	/*methode qui s'exécute lors de la creation de l'objet donc: $person1 = new Person;*/
-	public function __construct($name){
+	public function __construct(string $name){
 		$this->nameWolf = $name;
-		echo "Woaf dit ".$name;
 	}
 
 	/*Creation d'un mutateur*/
-	public function setName($pnameWolf){
+	public function setName(string $pnameWolf){
 		$this->nameWolf = $pnameWolf;
 	}
 
 	/*Creation d'un getteur (accesseur)*/
-	public function getName(){
+	public function getNameWolf(){
 		return $this->nameWolf;
 	}
 }
 
-$person1 = new Person("John", "Scena");
+
 //$person1->firstname = "John";
 //$person1->lastname = "Snow";
 //$person1->setName("John", "Snow");
 //var_dump($person1->getName());
+
+
+$person1 = new Person("John", "Snow");
+
+$wolf = new Wolf("Doug");
+
+$person1->meetWolf($wolf);
+
 var_dump($person1->speak());
 
-$wolf = new Wolf("Bidule");
+
+
+
 //var_dump($wolf); 
+
 //$person2 = new Person;
 //$person2->setName("Aria", "Stark");
 //var_dump($person2);
